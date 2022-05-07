1efdff93f4de02ea7029b1d371043d055dd85970
Miguel <miguel@equalit.ie>
2022-05-06 22:33:02 -0500

    Draft using HTTPURLConnection

--- app/src/main/java/com/example/ouitest1/MainActivity.java
+++ app/src/main/java/com/example/ouitest1/MainActivity.java
@@ -6,6 +6,13 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,10 +22,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getURL(View view) {
-        EditText editText = (EditText) findViewById(R.id.url);
-        String url = editText.getText().toString();
+        EditText editText = (EditText) findViewById(R.id.url_text);
+        String url_str = editText.getText().toString();
 
-        Toast toast = Toast.makeText(this, "Loading: " + url, Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(this, "Loading: " + url_str, Toast.LENGTH_SHORT);
+        toast.show();
+
+        URL url = null;
+        try {
+            url = new URL(url_str);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        HttpURLConnection urlConnection = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setConnectTimeout(60);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int response_code = 0;
+        try {
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            response_code = urlConnection.getResponseCode();
+            toast.setText("Response: " + response_code);
+        } catch (IOException e) {
+            toast.setText("Error");
+            e.printStackTrace();
+        }
         toast.show();
     }
 
