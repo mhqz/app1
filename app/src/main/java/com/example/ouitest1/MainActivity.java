9eb6a702f41e0eaaf02426bdfaee5dde8a5d5d58
Miguel <miguel@equalit.ie>
2022-05-06 23:00:10 -0500

    Load the URL using an async OKHttp request

--- app/src/main/java/com/example/ouitest1/MainActivity.java
+++ app/src/main/java/com/example/ouitest1/MainActivity.java
@@ -5,6 +5,9 @@ import android.widget.Toast;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import okhttp3.*;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +23,31 @@ public class MainActivity extends AppCompatActivity {
 
         Toast toast = Toast.makeText(this, "Loading: " + url, Toast.LENGTH_SHORT);
         toast.show();
-    }
 
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            public void onResponse(Call call, Response response) throws IOException {
+                try (ResponseBody responseBody = response.body()) {
+                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+                    Headers responseHeaders = response.headers();
+                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                        System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+                    }
+
+                    toast.setText(responseBody.string());
+                    toast.show();
+                }
+            }
+        });
+
+    }
 }
