fcd6fb4b29f15d4df367254cd140dad720bbb7be
Miguel <miguel@equalit.ie>
2022-05-13 14:30:17 -0500

    Implement a TrustManager using Ouinet's CA

--- app/src/main/java/com/example/ouitest1/MainActivity.java
+++ app/src/main/java/com/example/ouitest1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ouitest1;
 
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,15 +12,23 @@ import ie.equalit.ouinet.Ouinet;
 import okhttp3.*;
 
 import javax.net.ssl.*;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
     private Ouinet ouinet;
+    private String ouinet_dir;
+    private static final String TAG = "OuinetTester";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
                 //.setDisableProxyAccess(true)
                 //.setDisableInjectorAccess(true)
                 .build();
+        ouinet_dir = config.getOuinetDirectory();
 
         ouinet = new Ouinet(this, config);
         ouinet.start();
@@ -68,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(this, "Loading: " + url, Toast.LENGTH_SHORT);
         toast.show();
 
-        OkHttpClient client = getUnsafeHttpClient();
+        OkHttpClient client = getOuinetHttpClient();
         Request request = new Request.Builder()
                 .url(url)
                 .build();
@@ -92,23 +102,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private OkHttpClient getUnsafeHttpClient() {
+    private OkHttpClient getOuinetHttpClient() {
         try {
-            TrustManager[] trustAllCerts = getTrustManager();
-            SSLSocketFactory sslSocketFactory = getSSLSocketFactory(trustAllCerts);
+            TrustManager[] trustManagers = getOuinetTrustManager();
 
             OkHttpClient.Builder builder = new OkHttpClient.Builder();
             builder.sslSocketFactory(
-                    sslSocketFactory,
-                    (X509TrustManager) trustAllCerts[0]);
-
-            // Bypass hostname verification
-            //builder.hostnameVerifier(new HostnameVerifier() {
-            //    @Override
-            //    public boolean verify(String hostname, SSLSession session) {
-            //        return true;
-            //    }
-            //});
+                    getSSLSocketFactory(trustManagers),
+                    (X509TrustManager) trustManagers[0]);
 
             // Proxy to ouinet service
             Proxy ouinetService= new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 8077));
@@ -119,33 +120,72 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private SSLSocketFactory getSSLSocketFactory(TrustManager[] trustAllCerts) throws NoSuchAlgorithmException, KeyManagementException {
-        // Install the all-trusting trust manager
+    private SSLSocketFactory getSSLSocketFactory(TrustManager[] trustManagers) throws NoSuchAlgorithmException, KeyManagementException {
         final SSLContext sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+        sslContext.init(null, trustManagers, new java.security.SecureRandom());
         return sslContext.getSocketFactory();
     }
 
-    private TrustManager[] getTrustManager() {
-        // Create a trust manager that does not validate certificate chains
-        final TrustManager[] trustAllCerts = new TrustManager[]{
-                new X509TrustManager() {
+    private TrustManager[] getOuinetTrustManager() throws NoSuchAlgorithmException, KeyStoreException, CertificateException, IOException {
+        return new TrustManager[]{new OuinetTrustManager()};
+    }
 
-                    @Override
-                    public void checkClientTrusted(java.security.cert.X509Certificate[] chain,
-                                                   String authType) {
-                    }
+    private class OuinetTrustManager implements X509TrustManager {
+        private X509TrustManager trustManager;
+        private Certificate ca;
 
-                    @Override
-                    public void checkServerTrusted(java.security.cert.X509Certificate[] chain,
-                                                   String authType) {
-                    }
-                    @Override
-                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                        return new java.security.cert.X509Certificate[]{};
-                    }
+        public OuinetTrustManager() throws NoSuchAlgorithmException, KeyStoreException, CertificateException, IOException {
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(getKeyStore());
+
+            for (TrustManager tm : tmf.getTrustManagers()) {
+                if (tm instanceof X509TrustManager) {
+                    trustManager = (X509TrustManager) tm;
+                    break;
                 }
-        };
-        return trustAllCerts;
+            }
+        }
+
+        private KeyStore getKeyStore() throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException {
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStore.load(null, null);
+            keyStore.setCertificateEntry("ca", getCertificateAuthority());
+            return keyStore;
+        }
+
+        private Certificate getCertificateAuthority() throws CertificateException {
+            InputStream caInput = null;
+            try {
+                caInput = new FileInputStream(ouinet_dir + "/ssl-ca-cert.pem");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            ca = cf.generateCertificate(caInput);
+            return ca;
+        }
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            for (X509Certificate cert : chain) {
+                Log.d(TAG, "Server Cert Issuer: " + cert.getIssuerDN().getName() + " " + cert.getSubjectDN().getName());
+            }
+            for (X509Certificate cert : trustManager.getAcceptedIssuers()) {
+                Log.d(TAG, "Client Trusted Issuer: " + cert.getIssuerDN().getName());
+            }
+
+            trustManager.checkServerTrusted(chain, authType);
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[]{(X509Certificate) ca};
+        }
+
     }
 }
